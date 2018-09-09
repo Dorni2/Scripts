ce90b7c4258fecfe2a682591745e5f9d8d1aff67
Dor <dorni2@gmail.com>
2018-09-09 15:26:43 +0300

    added ffmpeg scripts

---
+++ AddAsStream.ps1
@@ -0,0 +1,22 @@
+﻿$path = Read-Host -Prompt "Enter path for folder"
+cd $path
+New-Item -ItemType Directory -Path .\Output -ErrorAction Ignore
+$ffmpeg = "D:\Dor\ffmpeg-20180708-3a2d21b-win64-static\bin\ffmpeg.exe"
+$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+$Files = Get-ChildItem -Path . 
+$Videos = $Files | where {$_ -like "*.mkv"}
+$SrtSubtitles = $Files | where {$_ -like "*.srt"}
+foreach  ($vid in $Videos)
+{
+    $SrtSub = $vid.Name.Replace('.mkv', '.srt')
+    $MkvOut = $path + '\Output\' + $vid.Name.Replace('.mkv', '_SubStreamed.mkv')
+    &$ffmpeg -i $vid.Name -sub_charenc WINDOWS-1255 -i $SrtSub -map 0:v -map 0:a -c copy -map 1 -c:s:0 srt -metadata:s:s:0 language=heb $MkvOut
+    "Elapsed time: " + $Stopwatch.Elapsed.ToString()
+    ##&$ffmpeg -i $vid.Name -sub_charenc WINDOWS-1255 -i $SrtSub -c:v copy -c:a copy -c:s copy $MkvOut
+}
+$Stopwatch.Stop()
+$FinishMessage = $Videos.Count.ToString() +  " Videos was rendered in " + $Stopwatch.Elapsed.ToString()
+$AvgMessage = "Avreage of " + ($Stopwatch.Elapsed.TotalSeconds / $Videos.Count).ToString() + " seconds per video"
+echo $FinishMessage
+echo $AvgMessage
+
---
+++ PrintOnVideo.ps1
@@ -0,0 +1,55 @@
+﻿$path = Read-Host -Prompt "Enter path for folder"
+cd $path
+New-Item -ItemType Directory -Path .\Output -ErrorAction Ignore
+$ffmpeg = "D:\Dor\ffmpeg-20180708-3a2d21b-win64-static\bin\ffmpeg.exe"
+$Files = Get-ChildItem -Path . 
+$Videos = $Files | where {$_ -like "*.mkv"}
+$SrtSubtitles = $Files | where {$_ -like "*.srt"}
+$SrtSubtitles | ForEach-Object {&$ffmpeg -i $_.FullName $_.FullName.Replace('srt','ass')}
+$AssSubtitles = Get-ChildItem -Path . | where {$_ -like "*.ass"}
+foreach  ($vid in $Videos)
+{
+    $AssSub = $vid.Name.Replace('.mkv', '.ass')
+    $MkvOut = $path + '\Output\' + $vid.Name.Replace('.mkv', '_subbed.mkv')
+    &$ffmpeg -i $vid.Name -vf ass=$AssSub $MkvOut
+}
+
+
+
+
+
+
+
+
+
+
+
+##$AssSubtitles = $Srtubtitles | ForEach-Object 
+##{
+##Write-Host $_.FullName;
+##$_.FullName.Replace('srt','ass');
+##}
+##foreach ($itm in $SrtSubtitles) 
+##{
+##    $AssSubtitles.ad
+##    $AssSubtitles.Add($itm.FullName.Replace('srt','ass'))
+##}
+
+
+##$Videos | ForEach-Object {&$ffmpeg -i $_.FullName -vf ass=$_.FullName.Replace('mkv','ass') $_.FullName.Replace('.mkv', '_subbed.mkv')}
+
+#foreach  ($vid in $Videos)
+#{
+#    $vid
+#    $AssSub = $vid.FullName.Replace('mkv','ass')
+#    $MkvOut = $vid.FullName.Replace('.mkv', '_subbed.mkv')
+#    &$ffmpeg -i $vid.FullName -vf ass=$AssSub $MkvOut
+#}
+
+##trying to escape
+
+
+
+#&$ffmpeg -i D:\Dor\Tv-Shows\s11\Two.and.a.Half.Men.S11E01.720p.5.1Ch.Web-DL.ReEnc-DeeJayAhmed.mkv -vf ass=D:\Dor\Tv-Shows\s11\Two.and.a.Half.Men.S11E01.720p.5.1Ch.Web-DL.ReEnc-DeeJayAhmed.ass D:\Dor\Tv-Shows\s11\Two.and.a.Half.Men.S11E01.720p.5.1Ch.Web-DL.ReEnc-DeeJayAhmed_sub.mkv
+
+
