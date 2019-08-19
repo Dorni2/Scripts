fc670d234f2018029741b8144bc6ec0792947c67
Dor <dorni2@gmail.com>
2019-08-19 12:32:49 +0300

    AddAsStream update

--- AddAsStream.ps1
+++ AddAsStream.ps1
@@ -5,14 +5,17 @@ $ffmpeg = "D:\Dor\ffmpeg-20180708-3a2d21b-win64-static\bin\ffmpeg.exe"
 $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 $Files = Get-ChildItem -Path . 
 $Videos = $Files | where {$_ -like "*.mkv"}
+## Change the format here ----------^---------
 $SrtSubtitles = $Files | where {$_ -like "*.srt"}
 foreach  ($vid in $Videos)
 {
     $SrtSub = $vid.Name.Replace('.mkv', '.srt')
+    ## Change the format here -----^---------    
     $MkvOut = $path + '\Output\' + $vid.Name.Replace('.mkv', '_SubStreamed.mkv')
+    ## Change the format here --------------------------^---------
+        ## output format cannot be changed - has to be mkv-----------^----------
     &$ffmpeg -i $vid.Name -sub_charenc WINDOWS-1255 -i $SrtSub -map 0:v -map 0:a -c copy -map 1 -c:s:0 srt -metadata:s:s:0 language=heb $MkvOut
     "Elapsed time: " + $Stopwatch.Elapsed.ToString()
-    ##&$ffmpeg -i $vid.Name -sub_charenc WINDOWS-1255 -i $SrtSub -c:v copy -c:a copy -c:s copy $MkvOut
 }
 $Stopwatch.Stop()
 $FinishMessage = $Videos.Count.ToString() +  " Videos was rendered in " + $Stopwatch.Elapsed.ToString()
